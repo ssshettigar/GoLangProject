a5c558b845d115dc8a078661a6a606cf6d7df029
Srikanth Shettigar <SrikanthShettigar@gmail.com>
2021-01-15 08:46:08 -0500

    Go Learning- Sample Projects

---
+++ CARDS/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//variation in Declaration
+	//var card string = "Ace of Spades"
+	card := "Ace of Spades"  //declaring card variable as string
+	card = "Ace of Diamonds" //assigning a new value to card variable
+	fmt.Println(card)
+}
---
+++ HelloWorld/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt" // standard library - fmt= format - input and output I/O functions
+
+func main() {
+	fmt.Println("Hi! there")
+}
