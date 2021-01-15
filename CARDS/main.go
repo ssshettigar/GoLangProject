469be45b932de303ab1573aefe98b2f993412016
Srikanth Shettigar <SrikanthShettigar@gmail.com>
2021-01-15 08:59:12 -0500

    variable declaration and initialization

--- CARDS/main.go
+++ CARDS/main.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
+var deckSize int //variable can be declared outside and you can't assign value here
+
 func main() {
 	//variation in Declaration
 	//var card string = "Ace of Spades"
-	card := "Ace of Spades"  //declaring card variable as string
+	card := "Ace of Spades"  //declaring a new variable as string
 	card = "Ace of Diamonds" //assigning a new value to card variable
 	fmt.Println(card)
+
+	deckSize = 50
+	fmt.Println(deckSize)
 }
