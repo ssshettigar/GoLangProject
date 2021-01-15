bcd708651eb41cfdd97052d89f9e23ce2d261e03
Srikanth Shettigar <SrikanthShettigar@gmail.com>
2021-01-15 09:07:58 -0500

    function call in go with return type string

--- CARDS/main.go
+++ CARDS/main.go
@@ -2,15 +2,16 @@ package main
 
 import "fmt"
 
-var deckSize int //variable can be declared outside and you can't assign value here
-
 func main() {
 	//variation in Declaration
-	//var card string = "Ace of Spades"
-	card := "Ace of Spades"  //declaring a new variable as string
-	card = "Ace of Diamonds" //assigning a new value to card variable
+
+	card := newCard() //declaring a new variable as string
+
 	fmt.Println(card)
 
-	deckSize = 50
-	fmt.Println(deckSize)
+}
+
+// new function newCard which return string value
+func newCard() string {
+	return "Five of Diamonds"
 }
