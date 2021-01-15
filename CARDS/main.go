172d530556244c9701d0f607c2c00bc226d385d0
Srikanth Shettigar <SrikanthShettigar@gmail.com>
2021-01-15 09:46:20 -0500

    slices and for loop in GO

--- CARDS/main.go
+++ CARDS/main.go
@@ -3,15 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	//variation in Declaration
 
-	card := newCard() //declaring a new variable as string
-
-	fmt.Println(card)
+	//declaring slice
+	cards := []string{"Ace of Diamonds", newCard()}
+	//adding value to slice
+	cards = append(cards, "Six of Spades")
+	//iterating through slice
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 
 }
 
 // new function newCard which return string value
 func newCard() string {
-	return "Five of Diamonds"
+	return "Aces of Spades"
 }
